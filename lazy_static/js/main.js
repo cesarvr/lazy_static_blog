822f92df0bbfafde28ef374e36d4d448052a5d7f
Cesar Valdez <cesarv01@gmail.com>
2014-07-07 12:13:49 +0200

    CSS & Customized navigation

--- lazy_static/js/main.js
+++ lazy_static/js/main.js
@@ -5,5 +5,17 @@
 	
 	lazy.History.init(); //Listen for change in URL
 	
+
+
+	/*
+		Add custom routes. 
+	*/
+	lazy.History.add_page({
+		url: "#about",
+		resource:"template/about.html"
+
+	});
+
+
 	//lazy.test_cmp();
 })();
